6dba989095cebcf10455c80a3955b0f5b8263530
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2026-08-18 18:38:48 -0300

    fix: added SwiftTesting support

---
+++ PebblesTests/PebblesTests.swift
@@ -0,0 +1,18 @@
+//
+//  PebblesTests.swift
+//  PebblesTests
+//
+//  Created by Tiago Camargo Maciel dos Santos on 18/08/26.
+//
+
+import Testing
+
+struct PebblesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
