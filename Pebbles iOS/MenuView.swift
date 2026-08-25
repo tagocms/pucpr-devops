cb216a74b58a08ae0fb47195ae2c683b731141b0
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2026-08-25 19:44:09 -0300

    feat: add multiplayer support

--- Pebbles iOS/MenuView.swift
+++ Pebbles iOS/MenuView.swift
@@ -5,14 +5,32 @@
 //  Created by Tiago Camargo Maciel dos Santos on 25/08/26.
 //
 
+internal import Combine
 import SwiftUI
 
 struct MenuView: View {
+    @ObservedObject var playerModel: PlayerModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Which player are you?")
+            Button("1") {
+                playerModel.id = 1
+            }
+            Button("2") {
+                playerModel.id = 2
+            }
+        }
+    }
+}
+
+class PlayerModel: ObservableObject {
+    @Published var id: Int
+
+    init(_ id: Int = 0) {
+        self.id = id
     }
 }
 
 #Preview {
-    MenuView()
+    MenuView(playerModel: PlayerModel(0))
 }
