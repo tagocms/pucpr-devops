ee261e7cdaee18c89019221146673cd2d257bbd1
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2026-08-25 19:45:25 -0300

    fix: updating tests

--- PebblesTests/PebblesTests.swift
+++ PebblesTests/PebblesTests.swift
@@ -15,7 +15,7 @@ import UIKit
 struct GameSceneTests {
     @Test func `ball and ground nodes exist after setupScene called`() async throws {
         let vc = await GameViewController()
-        let gameScene = await GameScene.newGameScene(size: vc.view.bounds.size)
+        let gameScene = await GameScene.newGameScene(size: vc.view.bounds.size, newPlayerID: 1)
         
         #expect(await gameScene.children.isEmpty == true)
         await gameScene.didMove(to: .init())
