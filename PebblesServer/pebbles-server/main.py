ca5a5843c71ec3e16cf76e2b62bc9bdec9f5a56d
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2026-08-25 17:50:58 -0300

    feat: added fastapi server to the repo

---
+++ PebblesServer/pebbles-server/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Position(BaseModel):
+    x: int
+    y: int
+
+INITIAL_POSITION = {'x': 0, 'y': 0}
+
+POSITIONS = {
+        1: Position(**INITIAL_POSITION),
+        2: Position(**INITIAL_POSITION)
+        }
+
+@app.get("/position/{player_id}")
+def get_player_position(player_id: int):
+    return {"player_id": player_id, "position": POSITIONS[player_id]}
+
+@app.post("/position/{player_id}")
+def update_player_position(player_id: int, position: Position):
+    POSITIONS[player_id] = position
+    return {"player_id": player_id, "position": POSITIONS[player_id]}
